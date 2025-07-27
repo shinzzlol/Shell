e4fac07ab220985c54150d8df009d0d45bb996aa
shinzzlol <atgtamil1@gmail.com>
2025-07-27 22:08:22 +0530

    Update pcf.sh

--- pcf.sh
+++ pcf.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-echo Starting Pterodactyl Installer Script 🚀
-echo Use your panel hostname, Don't assume SSL, Don't use LetsEncryt, Don't allow firewall too 
+echo "Starting Pterodactyl Installer Script 🚀"
+echo "Use your panel hostname, Don't assume SSL, Don't use LetsEncryt, Don't allow firewall too"
 
-echo Loading..
-echo Loading..
-echo Loading..
-echo Loading on to installation
+sudo bash <(curl -s https://pterodactyl-installer.se)
 
-bash <(curl -s https://pterodactyl-installer.se)
-
-echo Loading..
-echo Loading..
-echo Making boot certificates 🏓
-
-
+echo "Making boot certificates 🏓"
 mkdir -p /etc/certs
 cd /etc/certs
 openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=NA/ST=NA/L=NA/O=NA/CN=Generic SSL Certificate" -keyout privkey.pem -out fullchain.pem
 
-echo Loading..
-echo Loading..
-echo Installing Cloudflared Tunnel Dependencies 💻
-
+echo "Installing Cloudflared Tunnel Dependencies 💻"
 # Add cloudflare gpg key
 sudo mkdir -p --mode=0755 /usr/share/keyrings
 curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
@@ -33,14 +21,7 @@ echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudf
 # install cloudflared
 sudo apt-get update && sudo apt-get install cloudflared
 
-echo Loading..
-echo Loading..
-echo Loading..
-echo Loading..
-echo Loading..
-
-
-echo Create a tunnel in Zero Trust and copy the auth command and paste it and your work have done ✨
-echo Make a pubic hostname with http service, that must forward to 80
-echo Make a pubic hostname with https service, that must forward to 8443 with NoTLS Verify.
-echo Configure the node and then you're done!
+echo "Create a tunnel in Zero Trust and copy the auth command and paste it and your work is done ✨"
+echo "Make a pubic hostname with http service, that must forward to 80"
+echo "Make a pubic hostname with https service, that must forward to 8443 with NoTLS Verify."
+echo "Configure the node and then your done!"
